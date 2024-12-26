2ff645ee73b511a7147ae14c48ee7f88a3189a22
LucasFA <23667494+LucasFA@users.noreply.github.com>
2024-12-26 15:37:17 +0000

    Basic libslimbook package

--- pkgs/libslimbook/default.nix
+++ pkgs/libslimbook/default.nix
@@ -1,9 +1,46 @@
-{ stdenv }:
+{
+  stdenv,
+  lib,
+  pkgs,
+  fetchFromGitHub,
+  pkg-config,
+  meson,
+  ninja,
+  #glib,
+  #libintl,
+}:
 
 stdenv.mkDerivation rec {
-  pname = "";
-  version = "1.0";
-  src = ./.;
-  buildPhase = "echo echo Hello World > example";
-  installPhase = "install -Dm755 example $out";
+  pname = "libslimbook";
+  version = "1.17.0";
+
+  src = fetchFromGitHub {
+    owner = "Slimbook-Team";
+    repo = pname;
+    rev = "${version}";
+    sha256 = "sha256-c8WAWrLST99CBzisFAIXnP7LYhHKisCWg4g/ZunrN+Y=";
+  };
+  enableParallelBuilding = true;
+
+  nativeBuildInputs = with pkgs; [
+    gcc
+    # make
+    pkg-config
+    # binutils
+    meson
+    ninja
+  ];
+  buildInputs = [ ];
+  # buildInputs = [ glib libintl ]
+  #buildPhase = "echo Hello World";
+
+  # buildPhase = "echo echo Hello World > example";
+  # installPhase = "install -Dm755 example $out";
+  meta = with lib; {
+    broken = false;
+    description = "";
+    homepage = "https://github.com/Slimbook-Team/${pname}";
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
+  };
 }
