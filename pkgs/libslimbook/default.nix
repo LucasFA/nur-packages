4f825d8b83d9ee1f246b7173c25e6b585fe56208
LucasFA <23667494+LucasFA@users.noreply.github.com>
2025-07-28 21:29:53 +0100

    fmt

--- pkgs/libslimbook/default.nix
+++ pkgs/libslimbook/default.nix
@@ -7,11 +7,16 @@
   pkg-config,
   meson,
   ninja,
-  libinputSupport ? true, libinput,
-  efibootmgrSupport ? true, efibootmgr,
-  usbutilsSupport ? true, usbutils,
-  pciutilsSupport ? true, pciutils,
-  flatpakSupport ? true, flatpak,
+  libinputSupport ? true,
+  libinput,
+  efibootmgrSupport ? true,
+  efibootmgr,
+  usbutilsSupport ? true,
+  usbutils,
+  pciutilsSupport ? true,
+  pciutils,
+  flatpakSupport ? true,
+  flatpak,
 }:
 
 stdenv.mkDerivation rec {
@@ -33,46 +38,51 @@ stdenv.mkDerivation rec {
     ninja
   ];
 
-  buildInputs = [ ] ++
-    lib.optional libinputSupport libinput ++
-    lib.optional efibootmgrSupport efibootmgr ++
-    lib.optional usbutilsSupport usbutils ++
-    lib.optional pciutilsSupport pciutils ++
-    lib.optional flatpakSupport flatpak;
-    # lib.optional qc71_slimbook_laptopSupport qc71_slimbook_laptop
+  buildInputs =
+    [ ]
+    ++ lib.optional libinputSupport libinput
+    ++ lib.optional efibootmgrSupport efibootmgr
+    ++ lib.optional usbutilsSupport usbutils
+    ++ lib.optional pciutilsSupport pciutils
+    ++ lib.optional flatpakSupport flatpak;
+  # lib.optional qc71_slimbook_laptopSupport qc71_slimbook_laptop
 
-  patches = [ ./flatpak.diff ./efi_and_lib.diff ];
-  postPatch = ''
-    substituteInPlace \
-      src/slimbookctl.cpp \
-        --replace-fail "/usr/libexec" "$out/libexec"
-   '' 
-+  ''
-    substituteInPlace \
-      slimbook-settings.service \
-      slimbook-sleep \
-        --replace-fail "/usr/bin" "$out/bin"
-  ''
-+ ''
-    substituteInPlace \
-      report.d/libinput \
-        --replace-fail "libinput" "${pkgs.libinput}/bin/libinput"
-  ''
-+ ''
-    substituteInPlace \
-      report.d/efiboot \
-        --replace-fail "efibootmgr" "${pkgs.efibootmgr}/bin/efibootmgr"
-  ''
-+ ''
-    substituteInPlace \
-      report.d/pci \
-        --replace-fail "lspci" "${pkgs.pciutils}/bin/lspci"
-  ''
-+ ''
-    substituteInPlace \
-      report.d/usb \
-        --replace-fail "lsusb" "${pkgs.usbutils}/bin/lsusb"
-  '';
+  patches = [
+    ./flatpak.diff
+    ./efi_and_lib.diff
+  ];
+  postPatch =
+    ''
+      substituteInPlace \
+        src/slimbookctl.cpp \
+          --replace-fail "/usr/libexec" "$out/libexec"
+    ''
+    + ''
+      substituteInPlace \
+        slimbook-settings.service \
+        slimbook-sleep \
+          --replace-fail "/usr/bin" "$out/bin"
+    ''
+    + ''
+      substituteInPlace \
+        report.d/libinput \
+          --replace-fail "libinput" "${pkgs.libinput}/bin/libinput"
+    ''
+    + ''
+      substituteInPlace \
+        report.d/efiboot \
+          --replace-fail "efibootmgr" "${pkgs.efibootmgr}/bin/efibootmgr"
+    ''
+    + ''
+      substituteInPlace \
+        report.d/pci \
+          --replace-fail "lspci" "${pkgs.pciutils}/bin/lspci"
+    ''
+    + ''
+      substituteInPlace \
+        report.d/usb \
+          --replace-fail "lsusb" "${pkgs.usbutils}/bin/lsusb"
+    '';
 
   meta = with lib; {
     description = "";
